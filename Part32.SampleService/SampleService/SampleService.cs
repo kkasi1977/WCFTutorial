3f5b8d5d114c079a52146f09d5b725a16bb12669
julian <kkasi1977@gmail.com>
2020-12-30 19:03:07 +0900

    Part32 working

---
+++ Part32.SampleService/SampleService/SampleService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace SampleService
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 클래스 이름 "SampleService"을 변경할 수 있습니다.
+    public class SampleService : ISampleService
+    {
+
+
+        #region ISampleService 멤버
+
+        string ISampleService.RequestReplyOperation()
+        {
+            DateTime dtStart = DateTime.Now;
+            Thread.Sleep(2000);
+            DateTime dtEnd = DateTime.Now;
+
+            return dtEnd.Subtract(dtStart).Seconds.ToString() + " seconds processing time";
+        }
+
+        string ISampleService.RequestReplyOperation_ThrowsException()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Part32.WindowsClient/WindowsClient/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsClient
+{
+    public partial class Form1 : Form
+    {
+        SampleService.SampleServiceClient client;
+        public Form1()
+        {
+            InitializeComponent();
+            client = new SampleService.SampleServiceClient();
+        }
+
+ 
+        private void btnRequestReplyOperation_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Request-Reply Operation Started @ " + DateTime.Now.ToString());
+            btnRequestReplyOperation.Enabled = false;
+            listBox1.Items.Add(client.RequestReplyOperation());
+            btnRequestReplyOperation.Enabled = true;
+            listBox1.Items.Add("Request-Reply Operation Completed @ " + DateTime.Now.ToString());
+            listBox1.Items.Add("");
+        }
+    }
+}
