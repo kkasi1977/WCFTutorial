9d03acc33d25970e03b2d07bfb3531d6b919be7d
julian <kkasi1977@gmail.com>
2020-12-31 19:19:31 +0900

    Part35 working

---
+++ Part35.DownloadService/DownloadService/DownloadService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DownloadService
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 클래스 이름 "DownloadService"을 변경할 수 있습니다.
+    public class DownloadService : IDownloadService
+    {
+        //C:\git\WCFTutorial\Data\OpenAPIGuide.pdf
+ 
+        public File DownloadDocument()
+        {
+            File file = new File();
+            file.Content = System.IO.File.ReadAllBytes(@"C:\git\WCFTutorial\Data\OpenAPIGuide.pdf");
+            file.Name = "OpenAPIGuide.pdf";
+
+            return file;
+        }
+ 
+    }
+}
---
+++ Part35.DownloadService/DownloadService/IDownloadService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace DownloadService
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 인터페이스 이름 "IDownloadService"을 변경할 수 있습니다.
+    [ServiceContract]
+    public interface IDownloadService
+    {
+        [OperationContract]
+        File DownloadDocument();
+    }
+
+    [DataContract]
+    public class File
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public byte[] Content { get; set; }
+    }
+}
