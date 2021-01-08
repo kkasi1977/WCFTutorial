992ae520551ea9a1b6eab581c3bc1dd4b4f8ded3
julian <kkasi1977@gmail.com>
2021-01-08 13:30:59 +0900

    Part53 finished

---
+++ Part53.SimpleService/SimpleService/SimpleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SimpleService
+{
+    // 참고: "리팩터링" 메뉴에서 "이름 바꾸기" 명령을 사용하여 코드 및 config 파일에서 클래스 이름 "SimpleService"을 변경할 수 있습니다.
+    public class SimpleService : ISimpleService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
